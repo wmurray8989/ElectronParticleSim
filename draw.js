2fddf4b065614128e205b2dbef4f0c4a1bb3d7dc
Billy Murray <wmurray8989@gmail.com>
2017-09-15 19:03:46 -0400

    Added random motion to particle with bounding.

--- draw.js
+++ draw.js
@@ -9,18 +9,32 @@ class Particle {
     this.r=r;
   }
 
+  //Draw the particle to the canvas
   draw(){
     ctx.beginPath();
     ctx.arc(this.x,this.y,this.r,0,2*Math.PI);
     ctx.fill();
   }
 
+  //Move the particle randomly
+  move(){
+    this.x+=10*Math.random()-5;
+    this.y+=10*Math.random()-5;
+
+    //bound the particle within the canvas
+    if (this.x<0) this.x=0;
+    if (this.x>500) this.x=500;
+    if (this.y<0) this.y=0;
+    if (this.y>500) this.y=500;
+  }
 }
+
+var p = new Particle(100,100,5);
 
 function mainRender(){
 
-  var p = new Particle(100,100,5);
   p.draw();
+  p.move();
 
   raf = window.requestAnimationFrame(mainRender);
 }
