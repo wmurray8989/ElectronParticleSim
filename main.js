a85902efade525d9de607a3e7518ab0e8fcdfca9
Billy Murray <wmurray8989@gmail.com>
2017-09-16 13:35:03 -0400

    Removed menu bar.

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ const path = require('path')
 let win
 
 function createWindow() {
-   win = new BrowserWindow({width: 750, height: 575}) 
+   win = new BrowserWindow({width: 750, height: 575})
+   win.setMenu(null);
    win.loadURL(url.format ({
       pathname: path.join(__dirname, 'index.html'),
       protocol: 'file:',
