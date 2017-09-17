d1fff1321aed30b8e89c8028ad5927e6233496fa
Billy Murray <wmurray8989@gmail.com>
2017-09-17 15:16:46 -0400

    Removed jumper specific wrapping. Now included in parent class.

--- js/particleJumper.js
+++ js/particleJumper.js
@@ -9,12 +9,5 @@ class ParticleJumper extends Particle{ constructor(x,y){
       this.x+=50*Math.random()-25;
       this.y+=50*Math.random()-25;
     }
-
-    //wrap the particle around the canvas
-    if (this.x<0) this.x=500;
-    if (this.x>500) this.x=0;
-    if (this.y<0) this.y=500;
-    if (this.y>500) this.y=0;
-
   }
 }
