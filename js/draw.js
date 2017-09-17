b498fb6a8beba8336bdb5f3fb8011ca8d82fa46c
Billy Murray <wmurray8989@gmail.com>
2017-09-16 22:00:08 -0400

    Moved particle class definition to its own file.

--- js/draw.js
+++ js/draw.js
@@ -2,35 +2,6 @@ var canvas = document.getElementById("drawCanvas");
 var ctx = canvas.getContext("2d");
 var raf;
 
-class Particle {
-  constructor(x,y,r,color){
-    this.x=x;
-    this.y=y;
-    this.r=r;
-    this.color=color;
-  }
-
-  //Draw the particle to the canvas
-  draw(){
-    ctx.fillStyle=this.color;
-    ctx.beginPath();
-    ctx.arc(this.x,this.y,this.r,0,2*Math.PI);
-    ctx.fill();
-  }
-
-  //Move the particle randomly
-  move(){
-    this.x+=10*Math.random()-5;
-    this.y+=10*Math.random()-5;
-
-    //bound the particle within the canvas
-    if (this.x<0) this.x=0;
-    if (this.x>500) this.x=500;
-    if (this.y<0) this.y=0;
-    if (this.y>500) this.y=500;
-  }
-}
-
 //Define array of particles
 var particles = [];
 
--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let win
 
 function createWindow() {
    win = new BrowserWindow({width: 750, height: 575})
-   win.setMenu(null);
+   //win.setMenu(null);
    win.loadURL(url.format ({
       pathname: path.join(__dirname, 'index.html'),
       protocol: 'file:',
